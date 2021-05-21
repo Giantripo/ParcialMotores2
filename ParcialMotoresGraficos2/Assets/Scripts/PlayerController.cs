5eaa31c16bf2d91f31f79ecade00ab0b38dbe5bc
Gian Franco <giancapo2011@gmail.com>
2021-05-21 00:47:24 -0300

    RayCast y primera Escene creada habitacion

---
+++ ParcialMotoresGraficos2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float rayDistance;
+   private new Transform camera;
+    void Start()
+    {
+        camera = transform.Find("Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(camera.position, camera.forward * rayDistance, Color.red);
+    }
+}
