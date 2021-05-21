0d1dfd2e8a53c5d793c6752687ae20ab8c882a45
Gian Franco <giancapo2011@gmail.com>
2021-05-21 02:09:23 -0300

    movimiento de la camara con el mouse

--- ParcialMotoresGraficos2/Assets/Scripts/PlayerController.cs
+++ ParcialMotoresGraficos2/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,47 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Vector2 sensibility;
     public float rayDistance;
    private new Transform camera;
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+
         camera = transform.Find("Camera");
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+        float hor = Input.GetAxis("Mouse X");
+        float ver = Input.GetAxis("Mouse Y");
+        if (hor !=0)
+        {
+            transform.Rotate(Vector3.up * hor * sensibility.x); 
+        }
+        if (hor != 0)
+        {
+            transform.Rotate(Vector3.left * ver * sensibility.y);
+            //float angle = (camera.localEulerAngles.x - ver * sensibility.y + 360) % 360;
+            //if(angle > 180)
+            //{
+            //    angle -= 360;
+            //}
+            //angle = Mathf.Clamp(angle, -80, 80);
+        }
+
         Debug.DrawRay(camera.position, camera.forward * rayDistance, Color.red);
+
+        if (Input.GetButtonDown("Interactable"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(camera.position, camera.forward, out hit, rayDistance, LayerMask.GetMask("Interactable")))
+            {
+                Debug.Log(hit.transform.name);
+            }
+        } 
     }
 }
