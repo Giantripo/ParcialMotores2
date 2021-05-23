5487a1aa1b287f5c4c834beee4d746f6fa05176c
Gian Franco <giancapo2011@gmail.com>
2021-05-22 23:33:41 -0300

    menu con letra listo

---
+++ ParcialMotoresGraficos2/Assets/Scripts/LetraCancionMovimiento.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetraCancionMovimiento : MonoBehaviour
+{
+     Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(0,5,0);
+    }
+}
