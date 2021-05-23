5cb2dbdcabea051f1861b87829b44b7d80512e22
Gian Franco <giancapo2011@gmail.com>
2021-05-23 04:00:46 -0300

    menu y nivel, creacion de killCarlos

---
+++ ParcialMotoresGraficos2/Assets/Scripts/MenuPortada.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPortada : MonoBehaviour
+{
+    //bool mov =false;
+    // float vel;
+    //Rigidbody rb;
+    //void Start()
+    //{
+    //    rb = GetComponent<Rigidbody>();
+    //    vel = 1;
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //    rb.velocity = new Vector3(1 * vel, rb.velocity.y, 0);
+
+    //    if (this.transform.position.y > 3f && mov == false)
+    //    {
+    //        vel = vel * -1;
+    //    }
+     
+    //}
+}
